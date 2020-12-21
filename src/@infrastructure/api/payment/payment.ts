3730ac104f3cb954a7279fd7241e0079ebc80b88
Ali Habibzadeh <ali.hci@icloud.com>
2020-12-21 16:26:56 +0000

    env

--- src/@infrastructure/api/payment/payment.ts
+++ src/@infrastructure/api/payment/payment.ts
@@ -1,6 +1,5 @@
 import { Construct } from "@aws-cdk/core";
 
-import { appConfig } from "../../../config/app-config/config.service";
 import { EnvVars } from "../../../config/app-config/env-vars.enum";
 import { Handlers } from "../../../handlers-list";
 import { LambdaFactory } from "../../@common/lambda.factory";
@@ -9,6 +8,6 @@ export default class AppPayment {
   constructor(private scope: Construct) {}
 
   public apiHandler = new LambdaFactory(this.scope, Handlers.createPaymentIntent, {
-    [EnvVars.stripeSecretKey]: appConfig.stripeSecretKey,
+    [EnvVars.stripeSecretKey]: process.env.STRIPE_SECRET_KEY || "",
   }).getLambda();
 }
--- src/config/app-config/schema.ts
+++ src/config/app-config/schema.ts
@@ -1,15 +1,15 @@
 import { EnvVars } from "./env-vars.enum";
 
 export const appConfigSchema = {
-  [EnvVars.region]: { doc: "AWS Region of the app", format: String, default: "", env: "region" },
-  [EnvVars.account]: { doc: "AWS accountId", format: String, default: "", env: "account" },
+  [EnvVars.region]: { format: String, default: "", env: "region" },
+  [EnvVars.account]: { format: String, default: "", env: "account" },
   [EnvVars.userPoolId]: { format: String, default: "", env: "userPoolId" },
   [EnvVars.userPoolClientId]: { format: String, default: "", env: "userPoolClientId" },
   [EnvVars.ticketsTableName]: { format: String, default: "", env: "ticketsTableName" },
   [EnvVars.drawsTableName]: { format: String, default: "", env: "drawsTableName" },
   [EnvVars.dataRetentionDays]: { format: Number, default: 7, env: "dataRetentionDays" },
-  [EnvVars.ticketPrice]: { format: Number, default: 7, env: "ticketPrice" },
-  [EnvVars.ticketCurrency]: { format: String, default: "USD", env: "ticketCurrency" },
+  [EnvVars.ticketPrice]: { format: Number, default: 5, env: "ticketPrice" },
+  [EnvVars.ticketCurrency]: { format: String, default: "GBP", env: "ticketCurrency" },
   [EnvVars.stripePublishableKey]: { format: String, default: "", env: "STRIPE_PUBLISHABLE_KEY" },
   [EnvVars.stripeSecretKey]: { format: String, default: "", env: "STRIPE_SECRET_KEY" },
 };
