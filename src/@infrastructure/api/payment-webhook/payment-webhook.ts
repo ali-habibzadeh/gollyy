f215a68d2da48560906232ac1a23b3bf69a84b61
Ali Habibzadeh <ali.hci@icloud.com>
2020-12-22 17:49:33 +0000

    raw

--- src/@infrastructure/api/payment-webhook/payment-webhook.ts
+++ src/@infrastructure/api/payment-webhook/payment-webhook.ts
@@ -22,7 +22,7 @@ export default class PaymentWebhook {
   });
 
   private integration = new LambdaIntegration(this.paymentWebhookHandler, {
-    requestTemplates: { "application/json": '{ rawBody: "$util.escapeJavaScript($util.base64Decode($input.body))" }' },
+    requestTemplates: { "application/json": '{ "rawBody": "$util.escapeJavaScript($util.base64Decode($input.body))" }' },
   });
 
   private defineApiMethods(): void {
